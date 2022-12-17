6982e78850f615513544de281b6c3aceca308a37
MirkoDuric <mirkodjuricczv@gmail.com>
2022-12-17 15:40:18 +0100

    Creating Input task field with unfinished functionality and adding styling to Input and Container

--- src/components/Container/Form/CreateTaskInput.js
+++ src/components/Container/Form/CreateTaskInput.js
@@ -1,5 +1,6 @@
-const CreateTaskInput = () => {
-  return (  );
+import React  from "react";
+const CreateTaskInput = (prop) => {
+  return (<input className={prop.className} type={prop.type} placeholder={prop.placeholder} value={prop.value} onChange={prop.onChange} onKeyDown={prop.onKeyDown}></input>);
 }
 
 export default CreateTaskInput;
--- src/components/Container/Form/Form.js
+++ src/components/Container/Form/Form.js
@@ -1,5 +1,18 @@
+import React, {useState} from "react";
+import CreateTaskInput from "./CreateTaskInput";
 const Form = () => {
-  return (  );
+  const [taskValue, getTaskValue] = useState("");
+  const handleInputTaskChange = (event) => {
+    getTaskValue(event.target.value, console.log(taskValue))
+  }
+ const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log('do validate');
+    }
+  };
+  return (
+  <CreateTaskInput className="task-input task-input" type="text" placeholder="What do you plan to do?" value={taskValue} onChange={handleInputTaskChange} onKeyDown={handleKeyDown} />
+  );
 }
 
 export default Form;
