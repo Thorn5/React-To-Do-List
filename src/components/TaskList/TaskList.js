ed73ac472191edfb0ff306547fc4f9d00d4a0d66
edb1230 <ebro1230@gmail.com>
2022-12-17 17:14:42 +0100

    minor changes

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,11 +2,13 @@ import Task from "./Task";
 
 const TaskList = (prop) => {
   let tasks = prop.tasks;
+
   const handleDelete = (taskId) => {
     const tasks = tasks.filter((t) => t.id !== taskId);
     tasks = { tasks };
     console.log("You Deleted Me.");
   };
+
   return tasks.map((task) => (
     <Task
       key={task.id}
