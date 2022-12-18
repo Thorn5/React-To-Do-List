50cb80eee33b8e51e1f3cebb2d35fcbfa245461b
edb1230 <ebro1230@gmail.com>
2022-12-18 15:42:53 +0100

    Delete Button Works - Don't Ask Me How

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,29 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CreateTaskInput from "./CreateTaskInput";
 import tasks from "../tasks";
 import TaskList from "../TaskList/TaskList";
-import Task from "../TaskList/Task";
 
 const Form = () => {
   const [taskValue, getTaskValue] = useState("");
+  let tasks2 = tasks;
+  const [tasksArray, newTasks] = useState(tasks2);
   const handleInputTaskChange = (event) => {
-    getTaskValue(event.target.value, console.log(taskValue))
-  }
+    getTaskValue(event.target.value);
+  };
   const [idCount, updateIdCount] = useState(1);
- const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
       updateIdCount(idCount + 1);
-      console.log("Radi")
-      tasks.push({task: taskValue,
-      id:idCount}); 
-      console.log(tasks);
+      tasks.push({ task: taskValue, id: idCount });
     }
   };
-  return (<div>
-  <CreateTaskInput className="task-input " type="text" placeholder="What do you plan to do?" value={taskValue} onChange={handleInputTaskChange} onKeyDown={handleKeyDown} />
-  <TaskList></TaskList>
-  </div>
+  const handleDelete = (taskId) => {
+    newTasks(
+      tasks.splice(
+        tasks.findIndex((task) => task.id == taskId.target.id),
+        1
+      )
+    );
+  };
+  return (
+    <div>
+      <CreateTaskInput
+        className="task-input "
+        type="text"
+        placeholder="What do you plan to do?"
+        value={taskValue}
+        onChange={handleInputTaskChange}
+        onKeyDown={handleKeyDown}
+        id={idCount}
+      />
+      <TaskList tasks={tasks} onDelete={handleDelete}></TaskList>
+    </div>
   );
-}
+};
 
 export default Form;
--- src/components/TaskList/Task.js
+++ src/components/TaskList/Task.js
@@ -2,7 +2,7 @@ const Task = (prop) => {
   return (
     <div className="task">
       <input type="text" value={prop.value} readOnly />
-      <button className="deleteButton" onClick={prop.onClick}>
+      <button className="deleteButton" onClick={prop.onDelete} id={prop.id}>
         Delete
       </button>
       <button className="editButton" onClick={prop.onClick}>
